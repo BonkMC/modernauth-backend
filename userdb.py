f8217a44cb2bb90d46198bf98c9a3d4211bfed38
shloakvatsyayan <shloak.vatsyayan@gmail.com>
2025-02-20 22:15:25 -0500

    Created flow for multiple servers to use this

--- userdb.py
+++ userdb.py
@@ -3,6 +3,7 @@ import json
 class UserDB:
     def __init__(self, filename="data/database.json"):
         self.filename = filename
+        # Structure: { server_id: { username: authdata, ... }, ... }
         self.data = {}
         self.load()
 
@@ -17,14 +18,17 @@ class UserDB:
         with open(self.filename, "w") as f:
             json.dump(self.data, f, indent=4)
 
-    def signup(self, username, authdata):
-        if username not in self.data:
-            self.data[username] = authdata
+    def signup(self, server_id, username, authdata):
+        if server_id not in self.data:
+            self.data[server_id] = {}
+        if username not in self.data[server_id]:
+            self.data[server_id][username] = authdata
             self.save()
             return True
         return False
 
-    def login(self, username, authdata):
-        if username in self.data and self.data[username].get("sub") == authdata.get("sub"):
+    def login(self, server_id, username, authdata):
+        if (server_id in self.data and username in self.data[server_id] and
+            self.data[server_id][username].get("sub") == authdata.get("sub")):
             return True
         return False
